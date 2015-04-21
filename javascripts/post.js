f537d05683827b109913d8b0059f27d7ede2de31
probitanima11 <probitanima11@gmail.com>
2015-04-21 16:10:25 +0900

    divTagEl 만드는 메소드 수정

--- javascripts/post.js
+++ javascripts/post.js
@@ -22,8 +22,8 @@ Post.prototype.getTagDivElement = function() {
 	var divTagEl = document.createElement("div");
 	for (var i = 0; i<this.tags.length; i++) {
 		var aTageEl = document.createElement("a");
-		aTageEl.setAttribute("class", "tag_"+this.tags[i].tag);
-		aTageEl.innerHTML = "#" + this.tags[i].tag;
+		aTageEl.setAttribute("class", "tag_"+this.tags[i]);
+		aTageEl.innerHTML = "#" + this.tags[i];
 		divTagEl.appendChild(aTageEl);
 		var aSpaceEl = document.createElement("a");
 		aSpaceEl.innerHTML = "&nbsp; &nbsp;"; 
