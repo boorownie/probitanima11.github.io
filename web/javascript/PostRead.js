235a66d96a80ed4e1cc00174aca2d13234efa3d9
probitanima11 <sunghyun.ryu@navercorp.com>
2016-06-13 01:05:52 +0900

    PostRead.js 분리

---
+++ web/javascript/PostRead.js
@@ -0,0 +1,21 @@
+($(document).ready(function () {
+    // 메뉴
+    $('header').load('../web/html/include/menu.html');
+    // 내용
+    $.ajax({
+        method : "get",
+        dataType : 'text',
+        url : "../resources/md/17.md",
+        success : function(data) {
+            // showdown.js 적용
+            var converter = new showdown.Converter(),
+                html = converter.makeHtml(data);
+            document.querySelector('#content').innerHTML = html;
+
+            // highlight.js 적용
+            $('pre code').each(function(i, block) {
+                hljs.highlightBlock(block);
+            });
+        }
+    });
+}))();
