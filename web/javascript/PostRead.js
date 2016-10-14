8cbef3e249c0304877bb83b8de1b7b0ff29ba62f
probitanima11 <sunghyun.ryu@navercorp.com>
2016-10-14 15:20:46 +0900

    구조 변경

--- web/javascript/PostRead.js
+++ web/javascript/PostRead.js
@@ -1,21 +1,35 @@
-($(document).ready(function () {
-    // 메뉴
-    $('header').load('/web/html/include/menu.html');
-    // 내용
+/**
+ * post 내용을 나타낸다.
+ */
+var PostRead = PostRead || {};
+
+/*
+ 포스트의 내용 로드
+ */
+PostRead.init = function() {
     $.ajax({
-        method : "get",
-        dataType : 'text',
-        url : "/resources"+location.pathname+".md",
-        success : function(data) {
+        method: "get",
+        dataType: 'text',
+        url: _makeUrl(location.pathname),
+        success: function (data) {
             // showdown.js 적용
-            var converter = new showdown.Converter({extensions: ['table']}),
-                html = converter.makeHtml(data);
+            var converter = new showdown.Converter({extensions: ['table']});
+            var html = converter.makeHtml(data);
             document.querySelector('#content').innerHTML = html;
 
             // highlight.js 적용
-            $('pre code').each(function(i, block) {
+            $('pre code').each(function (i, block) {
                 hljs.highlightBlock(block);
             });
         }
     });
-}));
+};
+
+/*
+    내용을 로드할 url을 추출한다
+ */
+PostRead._makeUrl = function(path) {
+    $('.sub_menu').on("click", "li", function (e) {
+        return "/" + path.split('/')[1] + "/md/" + path.split('/')[2].split('.')[0] + ".md";
+    });
+};
