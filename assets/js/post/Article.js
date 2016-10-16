6fc15ebf16a66642405c63a4493f2ea857e3336c
probitanima11 <sunghyun.ryu@navercorp.com>
2016-10-16 17:35:49 +0900

    구조 변경

--- assets/js/post/Article.js
+++ assets/js/post/Article.js
@@ -14,7 +14,7 @@ function Article(json) {
 
 Article.prototype.getImageUrl = function() {
 	if (this.image === "" || this.image === undefined) {
-		return "/assets/images/default_post" + (this.idx % 4) + ".jpg";
+		return "/images/default_post" + (this.idx % 4) + ".jpg";
 	}
 	return this.image;
 };
